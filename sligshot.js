c62e70ac75ef74b13da3927feb59cf72a106bd2e
RythamWadhawan <83757183+RythamWadhawan@users.noreply.github.com>
2021-06-13 18:02:17 +0530

    Add files via upload

---
+++ sligshot.js
@@ -0,0 +1,27 @@
+class Sligshot{
+
+    constructor(bodyA , bodyB){
+        
+        var options ={
+            length  :10,
+            stifness :0.04,
+            bodyA : bodyA,
+            bodyB : bodyB,
+
+
+
+        }    
+        
+        this.chain = Matter.Constraint.create (options) 
+
+        World.add(world,this.chain)
+
+
+    }
+      display(){
+
+      line (this.chain.bodyA.position.x,this.chain.bodyA.position.y,this.chain.bodyB.position.x,this.chain.bodyB.position.y)
+
+      }  
+
+}
